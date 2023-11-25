a7234d94fd673671a06540c04ae09dd1b14e6b66
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-11-25 20:39:07 +0900

    Initial

---
+++ src/main/java/zmq/zmq/ZmqApplication.java
@@ -0,0 +1,13 @@
+package zmq.zmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZmqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zmq/zmq/ZmqApplicationTests.java
@@ -0,0 +1,13 @@
+package zmq.zmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
